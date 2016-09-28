b3fa3e7f3781e160e0badb746d0873fe0fbb6951
Alexey Yaroshevich <zxqfox@gmail.com>
2016-09-28 23:06:53 +0300

    fix: concating order was wrong - make a declaration

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,15 @@ const gulp = require('gulp');
 const debug = require('gulp-debug');
 const concat = require('gulp-concat');
 
+const decl = [
+    'reset',
+    'page*',
+    'link*',
+    'button*'
+];
+
 gulp.task('build', function() {
-    return gulp.src('styles/*.css')
+    return gulp.src(decl.map(s => `styles/${s}.css`))
         .pipe(concat('main.css'))
         .pipe(gulp.dest('./dist'))
         .pipe(debug());
